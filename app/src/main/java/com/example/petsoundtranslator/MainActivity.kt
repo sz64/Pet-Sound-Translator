741cf41de04ea3e3e043c50103d828043d2e083b
Steven <sz64@bu.edu>
2019-10-20 13:39:10 -0400

    added new activity

--- app/src/main/java/com/example/petsoundtranslator/MainActivity.kt
+++ app/src/main/java/com/example/petsoundtranslator/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.petsoundtranslator
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log.d
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_start.setOnClickListener {
+            d( "button_start", "button pressed")
+            startActivity(Intent(this, Pagerecord::class.java))
+        }
     }
 }
